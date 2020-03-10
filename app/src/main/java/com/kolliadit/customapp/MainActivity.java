83d14a03b123570c80e0b68d5fa290334634a116
aditkolli <aditkk29@gmail.com>
2020-03-10 10:15:17 -0400

    Lab 06D: Tried to put in videos, but they didn't work. Started to implement scroll bar and put in shared preferences

--- app/src/main/java/com/kolliadit/customapp/MainActivity.java
+++ app/src/main/java/com/kolliadit/customapp/MainActivity.java
@@ -2,11 +2,14 @@ package com.kolliadit.customapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -19,55 +22,34 @@ public class MainActivity extends AppCompatActivity {
     EditText responseText;
     TextView displayText;
     TextView qText;
+    RadioGroup radio;
 
+    int[] scores;
+    SharedPreferences pref ; // 0 - for private mode
+    SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        pref= getApplicationContext().getSharedPreferences("MyPref", 0);
+        editor = pref.edit();
         setContentView(R.layout.activity_main);
-        sub=findViewById(R.id.clickButton);
-        responseText=findViewById(R.id.response2EditText);
-        displayText=findViewById(R.id.textBox);
-        qText=findViewById(R.id.questionBox);
+        sub=findViewById(R.id.btnDisplay);
+        final int[] scores=new int[]{0,0,0,0};
         qa=new HashMap<String,String>();
-        final String [] questions= new String[5];
-
-        qa.put("Who is the coolest person you know?","Adit");
-        qa.put("What city is TJ in?","Alexandria");
-        qa.put("What's the best computer?","A Mac");
-        qa.put("Who is the best player in the NBA?","Jordan Poole");
-        qa.put("What's the best baseball team?","The Dodgers");
-
+        for(int num=0;num<4;num++)
+            scores[num]=pref.getInt(""+num,0);
         sub.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v){
-                System.out.println("testing onclick:  Good Job");
-                Log.i("testButton","Good Job"+responseText.getText().toString());
-                double ind= Math.random()*5;
-                int index=(int)ind;
-                String que=questions[index]    ;
-                String an=qa.get(que);
-                String useran= String.valueOf(responseText.getText());
-                if (useran.equals(an))
-
-                    displayText.setText("Right answer. Proceed");
-                else
-                    displayText.setText("Wrong answer. Try again");
+            public void onClick(View v) {
+                int selectedId = radio.getCheckedRadioButtonId();
+                scores[selectedId]++;
+                editor.putInt(selectedId+"",scores[selectedId]);
+                editor.commit();
 
 
             }
         });
-        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
-                    if (responseText.getText().toString().equals("TJ")) {
-                        displayText.setText("TJ Rocks!");
-                        responseText.setText("");
-                        responseText.setHint("That's a good name.");
-                    }
-                }
-            }
-        });
+
 
 
     }
