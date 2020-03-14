1c74b8ec4e42d773e5a94f6ee52d333ea81535a9
aditkolli <aditkk29@gmail.com>
2020-03-14 08:12:56 -0400

    Lab 06E: Tried to implement fragment, but having issues still.

--- app/src/main/java/com/kolliadit/customapp/MainActivity.java
+++ app/src/main/java/com/kolliadit/customapp/MainActivity.java
@@ -1,23 +1,29 @@
 package com.kolliadit.customapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     Map<String,String> qa;
+    RadioButton val;
     Button sub;
     EditText responseText;
     TextView displayText;
@@ -29,28 +35,62 @@ public class MainActivity extends AppCompatActivity {
     SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
+        radio = (RadioGroup) findViewById(R.id.radiogrou);
         pref= getApplicationContext().getSharedPreferences("MyPref", 0);
         editor = pref.edit();
-        setContentView(R.layout.activity_main);
-        sub=findViewById(R.id.btnDisplay);
-        final int[] scores=new int[]{0,0,0,0};
-        qa=new HashMap<String,String>();
-        for(int num=0;num<4;num++)
-            scores[num]=pref.getInt(""+num,0);
-        sub.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
+
+        sub = findViewById(R.id.btnDisplay);
+        scores = new int[]{0, 0, 0, 0};
+        qa = new HashMap<String, String>();
+        for (int num = 0; num < 4; num++)
+            scores[num] = pref.getInt("" + num, 0);
+
+    }
+    public void onC(View v) {
+                System.out.println(radio);
                 int selectedId = radio.getCheckedRadioButtonId();
-                scores[selectedId]++;
-                editor.putInt(selectedId+"",scores[selectedId]);
+                val = (RadioButton) findViewById(selectedId);
+                String text = (String)( val.getResources().getResourceEntryName(selectedId));
+                text = text.substring(5);
+                if (text.equals("luka")) {
+                    selectedId = 0;
+                    scores[selectedId]++;
+                    editor.putInt(selectedId + "", scores[selectedId]);
+                }
+                if (text.equals("giannis")) {
+                    selectedId = 1;
+                    scores[selectedId]++;
+                    editor.putInt(selectedId + "", scores[selectedId]);
+                }
+                if (text.equals("poole")) {
+                    selectedId = 2;
+                    scores[selectedId]++;
+                    editor.putInt(selectedId + "", scores[selectedId]);
+                }
+                if (text.equals("lebron")) {
+                    selectedId = 3;
+                    scores[selectedId]++;
+                    editor.putInt(selectedId + "", scores[selectedId]);
+                }
                 editor.commit();
 
 
             }
-        });
 
 
 
+
+
+}
+class ArticleFragment extends Fragment {
+    // The onCreateView method is called when Fragment should create its View object hierarchy,
+    // either dynamically or via XML layout inflation.
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        // Defines the xml file for the fragment
+        return inflater.inflate(R.layout.fragment, parent, false);
     }
+
 }
